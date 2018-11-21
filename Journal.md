9feae4d35457b7a7708e65b0f9908b4586ab7b1f
曹洋笛 <2904428882@qq.com>
2018-11-22 07:56:01 +0800

    v0.0
    Only with simple map

---
+++ Journal.md
@@ -0,0 +1,16 @@
+# 开发日志
+
+### 2018.11.20
+
+实现原生地图的定位追踪功能
+
+### 2018.11.21
+
+改用高德地图SDK以获得更加个性化的展示
+
+例如：自定义当前位置小蓝点，自定义动画等等
+
+参考：https://lbs.amap.com/api/ios-sdk/gettingstarted
+
+然而失败了（linker command failed with exit code 1 (use -v to see invocation)），初步怀疑与版本问题有关
+
---
+++ MoeGirls Go v0.0/MoeGirlsGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoeGirlsGo
+//
+//  Created by 曹洋笛 on 2018/11/19.
+//  Copyright © 2018年 AldebaRain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
